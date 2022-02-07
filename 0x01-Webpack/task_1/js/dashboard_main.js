182e24c741d978b04322a7ce424f6a01d3150694
SneyderHOL <1507@holbertonschool.com>
2022-02-06 19:55:41 -0500

    [UPDATE]: fix task 1

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -2,7 +2,7 @@ import $ from "jquery";
 import _ from 'lodash';
 
 $('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the studnts</p>");
+$('body').append("<p>Dashboard data for the students</p>");
 $('body').append("<button>Click here to get started</button>");
 $('body').append("<p id='count'></p>");
 $('body').append("<p>Copyright - Holberton School</p>");
@@ -11,7 +11,7 @@ let count = 0;
   
 function updateCounter() {
   count += 1;
-  $('#count').text(`${count.toString()} clicks on the button`);
+  $('#count').text(`${count} clicks on the button`);
 }
 
-$('button').on('click', _.debounce(updateCounter));
+$('button').on('click', _.debounce(updateCounter, 500));
